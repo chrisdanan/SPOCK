178a1f544d37937afb0e383eaa9df615a4a67107
Christopher Danan <chrisddan@gmail.com>
2015-03-18 15:23:16 -0700

    Set up skeleton for app.js.

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var http = require("http"),
 	server;
 
 server = http.createServer(function(req, res){
-	res.writeHead(200, {"Content-Type" : "text/plain"});
-	res.end("Hello");
+	res.writeHead(200, {"Content-Type" : "application/json"});
+	res.write('{"test" : "working"}');
+	res.end();
 });
 
 server.listen(3000);
