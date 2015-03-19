17276fbde3be1ce9527c861935cd3fcdc9ecf375
Christopher Danan <chrisddan@gmail.com>
2015-03-18 18:31:22 -0700

    Fix errors detected by JSHint: *Fix format of JSON strings *Add "use strict" *Place function result in correct place.

--- app.js
+++ app.js
@@ -5,12 +5,21 @@
  *Modified: March 18, 2015
  *Purpose: It's Rock, Paper, Scissors, Lizard, Spock!!!
  *References:
-  Dennis Valbuena
-  Sarah Lee
-  Mario Andrade
-  Calvin 
-  samkass.com/theories/RPSSL.html
+   Consultants:
+	  Dennis Valbuena
+	  Sarah Lee
+	  Mario Andrade
+	  Calvin 
+  How to play Rock, Paper, Scissors, Lizard, Spock:
+  	samkass.com/theories/RPSSL.html
+  How to correctly format JSON objects:
+  	http://stackoverflow.com/questions/2422983/returning-json-object-from-an-asp-net-page
  */
+
+// Server-side code
+/* jshint node: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, strict: true, undef: true, unused: true */
+
+"use strict";
 
 var http = require("http"),
 	server,
@@ -24,6 +33,169 @@ function randomChoice(){
 		random = Math.floor(Math.random() * 5);
 
 	return choices[random];
+}
+
+//Purpose: Calculate the results of the game.
+function result(playerChoice, req, res){
+	var serverChoice = randomChoice();
+	console.log("PLAYER CHOICE: " + playerChoice);
+	console.log("SERVER CHOICE: " + serverChoice);
+	if(playerChoice === "rock"){
+		//Player LOSE
+		if(serverChoice === "spock" || serverChoice === "paper"){
+			losses++;
+			console.log("******************************");
+			console.log("Lose");
+			console.log("Number of losses: " + losses);
+			console.log("Number of wins: " + wins);
+			console.log("Number of ties: " + ties);
+			console.log("******************************");
+			res.write("{\"outcome\": \"loss\", \"wins\": \"" + wins + "\", \"losses\": \"" + losses + "\", \"ties\": \"" + ties + "\"}");
+			//res.write('{"outcome": "loss", "wins": "' + wins + '", "losses": "' + losses + '", "ties": "' + ties + '"}');
+		//Player WIN
+		} else if(serverChoice === "lizard" || serverChoice === "scissors"){
+			wins++;
+			console.log("******************************");
+			console.log("Win");
+			console.log("Number of losses: " + losses);
+			console.log("Number of wins: " + wins);
+			console.log("Number of ties: " + ties);
+			console.log("******************************");
+			res.write("{\"outcome\": \"win\", \"wins\": \"" + wins + "\", \"losses\": \"" + losses + "\", \"ties\": \"" + ties + "\"}");
+		//TIE
+		} else if(serverChoice === "rock"){
+			ties++;
+			console.log("******************************");
+			console.log("Tie");
+			console.log("Number of losses: " + losses);
+			console.log("Number of wins: " + wins);
+			console.log("Number of ties: " + ties);
+			console.log("******************************");
+			res.write("{\"outcome\": \"tie\", \"wins\": \"" + wins + "\", \"losses\": \"" + losses + "\", \"ties\": \"" + ties + "\"}");
+		}
+	} else if(playerChoice === "paper"){
+		//Player LOSE
+		if(serverChoice === "scissors" || serverChoice ==="lizard"){
+			losses++;
+			console.log("******************************");
+			console.log("Lose");
+			console.log("Number of losses: " + losses);
+			console.log("Number of wins: " + wins);
+			console.log("Number of ties: " + ties);
+			console.log("******************************");
+			res.write("{\"outcome\": \"loss\", \"wins\": \"" + wins + "\", \"losses\": \"" + losses + "\", \"ties\": \"" + ties + "\"}");
+		//Player WIN
+		} else if(serverChoice === "spock" || serverChoice ==="rock"){
+			wins++;
+			console.log("******************************");
+			console.log("Win");
+			console.log("Number of losses: " + losses);
+			console.log("Number of wins: " + wins);
+			console.log("Number of ties: " + ties);
+			console.log("******************************");
+			res.write("{\"outcome\": \"win\", \"wins\": \"" + wins + "\", \"losses\": \"" + losses + "\", \"ties\": \"" + ties + "\"}");
+		//TIE
+		} else if(serverChoice === "paper"){
+			ties++;
+			console.log("******************************");
+			console.log("Tie");
+			console.log("Number of losses: " + losses);
+			console.log("Number of wins: " + wins);
+			console.log("Number of ties: " + ties);
+			console.log("******************************");
+			res.write("{\"outcome\": \"tie\", \"wins\": \"" + wins + "\", \"losses\": \"" + losses + "\", \"ties\": \"" + ties + "\"}");
+		}
+	} else if(playerChoice === "scissors"){
+		if(serverChoice === "rock" || serverChoice ==="spock"){
+			losses++;
+			console.log("******************************");
+			console.log("Lose");
+			console.log("Number of losses: " + losses);
+			console.log("Number of wins: " + wins);
+			console.log("Number of ties: " + ties);
+			console.log("******************************");
+			res.write("{\"outcome\": \"loss\", \"wins\": \"" + wins + "\", \"losses\": \"" + losses + "\", \"ties\": \"" + ties + "\"}");
+		} else if(serverChoice === "paper" || serverChoice === "lizard"){
+			wins++;
+			console.log("******************************");
+			console.log("Win");
+			console.log("Number of losses: " + losses);
+			console.log("Number of wins: " + wins);
+			console.log("Number of ties: " + ties);
+			console.log("******************************");
+			res.write("{\"outcome\": \"win\", \"wins\": \"" + wins + "\", \"losses\": \"" + losses + "\", \"ties\": \"" + ties + "\"}");
+		} else if(serverChoice === "scissors"){
+			ties++;
+			console.log("******************************");
+			console.log("Tie");
+			console.log("Number of losses: " + losses);
+			console.log("Number of wins: " + wins);
+			console.log("Number of ties: " + ties);
+			console.log("******************************");
+			res.write("{\"outcome\": \"tie\", \"wins\": \"" + wins + "\", \"losses\": \"" + losses + "\", \"ties\": \"" + ties + "\"}");
+		}
+	} else if(playerChoice === "lizard"){
+		if(serverChoice === "rock" || serverChoice === "scissors"){
+			losses++;
+			console.log("******************************");
+			console.log("Lose");
+			console.log("Number of losses: " + losses);
+			console.log("Number of wins: " + wins);
+			console.log("Number of ties: " + ties);
+			console.log("******************************");
+			res.write("{\"outcome\": \"loss\", \"wins\": \"" + wins + "\", \"losses\": \"" + losses + "\", \"ties\": \"" + ties + "\"}");
+		} else if(serverChoice === "paper" || serverChoice === "spock"){
+			wins++;
+			console.log("******************************");
+			console.log("Win");
+			console.log("Number of losses: " + losses);
+			console.log("Number of wins: " + wins);
+			console.log("Number of ties: " + ties);
+			console.log("******************************");
+			res.write("{\"outcome\": \"win\", \"wins\": \"" + wins + "\", \"losses\": \"" + losses + "\", \"ties\": \"" + ties + "\"}");
+		} else if(serverChoice === "lizard"){
+			ties++;
+			console.log("******************************");
+			console.log("Tie");
+			console.log("Number of losses: " + losses);
+			console.log("Number of wins: " + wins);
+			console.log("Number of ties: " + ties);
+			console.log("******************************");
+			res.write("{\"outcome\": \"tie\", \"wins\": \"" + wins + "\", \"losses\": \"" + losses + "\", \"ties\": \"" + ties + "\"}");
+		}
+	} else if(playerChoice === "spock"){
+		if(serverChoice === "lizard" || serverChoice === "paper"){
+			losses++;
+			console.log("******************************");
+			console.log("Lose");
+			console.log("Number of losses: " + losses);
+			console.log("Number of wins: " + wins);
+			console.log("Number of ties: " + ties);
+			console.log("******************************");
+			res.write("{\"outcome\": \"loss\", \"wins\": \"" + wins + "\", \"losses\": \"" + losses + "\", \"ties\": \"" + ties + "\"}");
+		} else if(serverChoice === "rock" || serverChoice === "scissors"){
+			wins++;
+			console.log("******************************");
+			console.log("Win");
+			console.log("Number of losses: " + losses);
+			console.log("Number of wins: " + wins);
+			console.log("Number of ties: " + ties);
+			console.log("******************************");
+			res.write("{\"outcome\": \"win\", \"wins\": \"" + wins + "\", \"losses\": \"" + losses + "\", \"ties\": \"" + ties + "\"}");
+		} else if(serverChoice === "spock"){
+			ties++;
+			console.log("******************************");
+			console.log("Tie");
+			console.log("Number of losses: " + losses);
+			console.log("Number of wins: " + wins);
+			console.log("Number of ties: " + ties);
+			console.log("******************************");
+			res.write("{\"outcome\": \"tie\", \"wins\": \"" + wins + "\", \"losses\": \"" + losses + "\", \"ties\": \"" + ties + "\"}");
+		}
+	} else{
+		console.log("Something went wrong");
+		res.write("{\"error\": \"playerChoice is invalid\"}");
+	}
 }
 
 //Purpose: Determine what choice player chose through the request's URL.
@@ -51,170 +223,8 @@ function getPost(req, res){
 		//res.write('{"choice" : "spock"}');
 		result("spock", req, res);
 	} else{
-		res.write('{"error" : "please put correct url"}');
+		res.write("{\"error\" : \"please put correct url\"}");
 		console.log("Client chose invalid url");
-	}
-}
-
-//Purpose: Calculate the results of the game.
-function result(playerChoice, req, res){
-	var serverChoice = randomChoice();
-	console.log("PLAYER CHOICE: " + playerChoice);
-	console.log("SERVER CHOICE: " + serverChoice);
-	if(playerChoice === "rock"){
-		//Player LOSE
-		if(serverChoice === "spock" || serverChoice === "paper"){
-			losses++;
-			console.log("******************************");
-			console.log("Lose");
-			console.log("Number of losses: " + losses);
-			console.log("Number of wins: " + wins);
-			console.log("Number of ties: " + ties);
-			console.log("******************************");
-			res.write('{"outcome": "loss", "wins": "' + wins + '", "losses": "' + losses + '", "ties": "' + ties + '"}');
-		//Player WIN
-		} else if(serverChoice === "lizard" || serverChoice === "scissors"){
-			wins++;
-			console.log("******************************");
-			console.log("Win");
-			console.log("Number of losses: " + losses);
-			console.log("Number of wins: " + wins);
-			console.log("Number of ties: " + ties);
-			console.log("******************************");
-			res.write('{"outcome": "win", "wins": "' + wins + '", "losses": "' + losses + '", "ties": "' + ties + '"}');
-		//TIE
-		} else if(serverChoice === "rock"){
-			ties++;
-			console.log("******************************");
-			console.log("Tie");
-			console.log("Number of losses: " + losses);
-			console.log("Number of wins: " + wins);
-			console.log("Number of ties: " + ties);
-			console.log("******************************");
-			res.write('{"outcome": "tie", "wins": "' + wins + '", "losses": "' + losses + '", "ties": "' + ties + '"}');
-		}
-	} else if(playerChoice === "paper"){
-		//Player LOSE
-		if(serverChoice === "scissors" || serverChoice ==="lizard"){
-			losses++;
-			console.log("******************************");
-			console.log("Lose");
-			console.log("Number of losses: " + losses);
-			console.log("Number of wins: " + wins);
-			console.log("Number of ties: " + ties);
-			console.log("******************************");
-			res.write('{"outcome": "loss", "wins": "' + wins + '", "losses": "' + losses + '", "ties": "' + ties + '"}');
-		//Player WIN
-		} else if(serverChoice === "spock" || serverChoice ==="rock"){
-			wins++;
-			console.log("******************************");
-			console.log("Win");
-			console.log("Number of losses: " + losses);
-			console.log("Number of wins: " + wins);
-			console.log("Number of ties: " + ties);
-			console.log("******************************");
-			res.write('{"outcome": "win", "wins": "' + wins + '", "losses": "' + losses + '", "ties": "' + ties + '"}');
-		//TIE
-		} else if(serverChoice === "paper"){
-			ties++;
-			console.log("******************************");
-			console.log("Tie");
-			console.log("Number of losses: " + losses);
-			console.log("Number of wins: " + wins);
-			console.log("Number of ties: " + ties);
-			console.log("******************************");
-			res.write('{"outcome": "tie", "wins": "' + wins + '", "losses": "' + losses + '", "ties": "' + ties + '"}');
-		}
-	} else if(playerChoice === "scissors"){
-		if(serverChoice === "rock" || serverChoice ==="spock"){
-			losses++;
-			console.log("******************************");
-			console.log("Lose");
-			console.log("Number of losses: " + losses);
-			console.log("Number of wins: " + wins);
-			console.log("Number of ties: " + ties);
-			console.log("******************************");
-			res.write('{"outcome": "loss", "wins": "' + wins + '", "losses": "' + losses + '", "ties": "' + ties + '"}');
-		} else if(serverChoice === "paper" || serverChoice === "lizard"){
-			wins++;
-			console.log("******************************");
-			console.log("Win");
-			console.log("Number of losses: " + losses);
-			console.log("Number of wins: " + wins);
-			console.log("Number of ties: " + ties);
-			console.log("******************************");
-			res.write('{"outcome": "win", "wins": "' + wins + '", "losses": "' + losses + '", "ties": "' + ties + '"}');
-		} else if(serverChoice === "scissors"){
-			ties++;
-			console.log("******************************");
-			console.log("Tie");
-			console.log("Number of losses: " + losses);
-			console.log("Number of wins: " + wins);
-			console.log("Number of ties: " + ties);
-			console.log("******************************");
-			res.write('{"outcome": "tie", "wins": "' + wins + '", "losses": "' + losses + '", "ties": "' + ties + '"}');
-		}
-	} else if(playerChoice === "lizard"){
-		if(serverChoice === "rock" || serverChoice === "scissors"){
-			losses++;
-			console.log("******************************");
-			console.log("Lose");
-			console.log("Number of losses: " + losses);
-			console.log("Number of wins: " + wins);
-			console.log("Number of ties: " + ties);
-			console.log("******************************");
-			res.write('{"outcome": "loss", "wins": "' + wins + '", "losses": "' + losses + '", "ties": "' + ties + '"}');
-		} else if(serverChoice === "paper" || serverChoice === "spock"){
-			wins++;
-			console.log("******************************");
-			console.log("Win");
-			console.log("Number of losses: " + losses);
-			console.log("Number of wins: " + wins);
-			console.log("Number of ties: " + ties);
-			console.log("******************************");
-			res.write('{"outcome": "win", "wins": "' + wins + '", "losses": "' + losses + '", "ties": "' + ties + '"}');
-		} else if(serverChoice === "lizard"){
-			ties++;
-			console.log("******************************");
-			console.log("Tie");
-			console.log("Number of losses: " + losses);
-			console.log("Number of wins: " + wins);
-			console.log("Number of ties: " + ties);
-			console.log("******************************");
-			res.write('{"outcome": "tie", "wins": "' + wins + '", "losses": "' + losses + '", "ties": "' + ties + '"}');
-		}
-	} else if(playerChoice === "spock"){
-		if(serverChoice === "lizard" || serverChoice === "paper"){
-			losses++;
-			console.log("******************************");
-			console.log("Lose");
-			console.log("Number of losses: " + losses);
-			console.log("Number of wins: " + wins);
-			console.log("Number of ties: " + ties);
-			console.log("******************************");
-			res.write('{"outcome": "loss", "wins": "' + wins + '", "losses": "' + losses + '", "ties": "' + ties + '"}');
-		} else if(serverChoice === "rock" || serverChoice === "scissors"){
-			wins++;
-			console.log("******************************");
-			console.log("Win");
-			console.log("Number of losses: " + losses);
-			console.log("Number of wins: " + wins);
-			console.log("Number of ties: " + ties);
-			console.log("******************************");
-			res.write('{"outcome": "win", "wins": "' + wins + '", "losses": "' + losses + '", "ties": "' + ties + '"}');
-		} else if(serverChoice === "spock"){
-			ties++;
-			console.log("******************************");
-			console.log("Tie");
-			console.log("Number of losses: " + losses);
-			console.log("Number of wins: " + wins);
-			console.log("Number of ties: " + ties);
-			console.log("******************************");
-			res.write('{"outcome": "tie", "wins": "' + wins + '", "losses": "' + losses + '", "ties": "' + ties + '"}');
-		}
-	} else{
-		console.log("Something went wrong");
-		res.write('{"error": "playerChoice is invalid"}');
 	}
 }
 
